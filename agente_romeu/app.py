000f30c077e1f51fecbc7053e8f6fe333c98d7e2
CdnCarvalho <claudenirfer@gmail.com>
2024-11-23 23:35:48 -0300

    Fix: test 1

--- agente_romeu/app.py
+++ agente_romeu/app.py
@@ -13,7 +13,7 @@ def webhook():
 
     # Capturar dados do Dialogflow e Telegram
     chat_id = data['originalDetectIntentRequest']['payload']['data']['chat']['id']
-    message_text = data['fulfillmentText']   # Texto retornado pelo Dialogflow ['queryResult']
+    message_text = data['queryResult']['fulfillmentText']   # Texto retornado pelo Dialogflow
 
     # Configurar a mensagem para o Telegram
     payload = {
@@ -22,10 +22,14 @@ def webhook():
         'parse_mode': 'Markdown'  # Pode ser 'MarkdownV2' para sintaxe mais avançada
     }
 
-    # Enviar a mensagem para o Telegram
-    response = requests.post(TELEGRAM_URL, data=payload)
+    # # Enviar a mensagem para o Telegram
+    # response = requests.post(TELEGRAM_URL, data=payload)
+    # Enviar a mensagem diretamente para o Telegram
+    requests.post(TELEGRAM_URL, data=payload)
 
-    return jsonify({'status': 'success'})
+    # Retornar um status vazio para que o Dialogflow não envie uma segunda resposta
+    return jsonify({})  # Isso evita a duplicação
+    # return jsonify({'status': 'success'})
 
 
 if __name__ == '__main__':
