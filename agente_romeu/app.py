3296263ee16655fa1ecec1498822f51b88fcd1ee
CdnCarvalho <claudenirfer@gmail.com>
2024-11-24 16:57:17 -0300

    Fix: test 2

--- agente_romeu/app.py
+++ agente_romeu/app.py
@@ -18,7 +18,7 @@ def webhook():
     # Configurar a mensagem para o Telegram
     payload = {
         'chat_id': chat_id,
-        'text': data['queryResult']['fulfillmentText'],
+        'text': data['queryResult']['fulfillmentMessages'],
         'parse_mode': 'Markdown'  # Pode ser 'MarkdownV2' para sintaxe mais avançada
     }
 
