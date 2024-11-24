2b1c8e842c64772c35b2b1699e45fb7252c975b1
CdnCarvalho <claudenirfer@gmail.com>
2024-11-24 13:47:14 -0300

    Fix: test 2

--- agente_romeu/app.py
+++ agente_romeu/app.py
@@ -12,13 +12,13 @@ def webhook():
     data = request.get_json(silent=True)
     print(data)
     # Capturar dados do Dialogflow e Telegram
-    chat_id = data['originalDetectIntentRequest']['payload']['data']['chat']['id']
-    # message_text = data['queryResult']['fulfillmentText']   # Texto retornado pelo Dialogflow
+    chat_id = data['originalDetectIntentRequest']['data']['chat']['id']
+    message_text = data['queryResult']['fulfillmentText']   # Texto retornado pelo Dialogflow
 
     # Configurar a mensagem para o Telegram
     payload = {
         'chat_id': chat_id,
-        # 'text': message_text,
+        'text': message_text,
         'parse_mode': 'Markdown'  # Pode ser 'MarkdownV2' para sintaxe mais avançada
     }
 
